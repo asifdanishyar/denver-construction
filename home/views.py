a94f166698d988e7ecdea7542d1a4a1a2926706f
asifdanishyar <asifdanishyar@yahoo.com>
2020-12-17 13:35:17 +0000

    cloned bootstrap theme

--- home/views.py
+++ home/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse ("hello world")
+    return render (request, 'home.html')
+
+
+def add (request):
+
+    val1 = int(request.POST["num1"])
+    val2 = int(request.POST["num2"])
+    res = val1 + val2
+
+    return render(request, "result.html", {'result': res})
